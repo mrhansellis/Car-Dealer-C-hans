1a5592a8e6bd3adfc66494edbca0baa0f18ca0dd
gramsower <110431307+gramsower@users.noreply.github.com>
2022-11-30 19:52:56 -0800

    add home index page and home controller module
    Co-authored by: Hans Ellis <bullhorn@gmail.com>
    C-authored-by: Remy Flores <remyfranciscoflores@gmail.com>

--- CarDealer.Tests/ModelTests/CarsTests.cs
+++ CarDealer.Tests/ModelTests/CarsTests.cs
@@ -8,38 +8,23 @@ namespace Inventory.Test
   [TestClass]
   public class InventoryTests : IDisposable
   {
-      public void Dispose()
-      {
-        Car.ClearAll();
-      }
-
-      [TestMethod]
-      public void CarConstrutor_CreatesInstanceOfCar_Car()
-      {
-        Car newCar = new Car("plymouth", "voyager", 1986, 500);
-        Assert.AreEqual(typeof(Car), newCar.GetType());
-      }
-      [TestMethod]
-      public void GetAll_ReturnsEmptyList_CarList()
-      {
-        List<Car> newInventory = new List<Car> { };
-        List<Car> result = Car.GetAll();
-        CollectionAssert.AreEqual(newInventory, result);
-      }
-    
-    
-    
-    /* [TestMethod]
-    public void CarConstructor_SetsPropertiesOfCar_Properties()
+    public void Dispose()
     {
-      string userInput1 = "plymouth";
-      string userInput2 = "voyager";
-      int userInput3 = 1986;
-      int userInput4 = 500;
-      Car newCar = new Car(userInput1, userInput2, userInput3, userInput4);
-      string result = newCar.Make;
-      Assert.AreEqual(userInput, result);
+      Car.ClearAll();
     }
-    [TestMethod] */
+
+    [TestMethod]
+    public void CarConstrutor_CreatesInstanceOfCar_Car()
+    {
+      Car newCar = new Car("plymouth", "voyager", 1986, 500);
+      Assert.AreEqual(typeof(Car), newCar.GetType());
+    }
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_CarList()
+    {
+      List<Car> newInventory = new List<Car> { };
+      List<Car> result = Car.GetAll();
+      CollectionAssert.AreEqual(newInventory, result);
+    }
   }
 }
--- CarDealer/Program.cs
+++ CarDealer/Program.cs
@@ -1,13 +1,28 @@
-using System;
-using Inventory.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace Dealer
+namespace CarDealer
 {
-  public class Program
+  class Program
   {
-    public static void Main()
+    static void Main(string[] args)
     {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+      builder.Services.AddControllersWithViews();
+
+      WebApplication app = builder.Build();
+
+      app.UseHttpsRedirection();
+      
+      app.UseRouting();
+
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
+
+      app.Run();
     }
   }
 }
